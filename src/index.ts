58e21c69b6a5f99761c9ec330b85db7d789d498e
Bekzat <rakhimbayevbekzat@gmail.com>
2023-03-07 12:14:21 +0300

    added interface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+// To find all property first I use js file and print the data which fetched using id. Here show me this properties, so I write all lof them to not lose data, even if we not use them. 
+interface Comic{
+    alt: string,
+    day: string,
+    img: string,
+    link: string,
+    month: string,
+    news: string,
+    num: number,
+    safe_title: string,
+    title: string,
+    transcript: string,
+    year: string,
+}
+
 
 function fetchID(mail : string) : Promise<number>  {
     const params : URLSearchParams = new URLSearchParams();
@@ -8,7 +23,7 @@ function fetchID(mail : string) : Promise<number>  {
     return fetch('https://fwd.innopolis.app/api/hw2?' + params.toString()).then(result => result.json());
 }
 
-function fetchImage(id : number) : Promise<any>  {
+function fetchImage(id : number) : Promise<Comic>  {
     const params : URLSearchParams= new URLSearchParams();
     if(id){
         params.append('num', String(id));
@@ -23,11 +38,11 @@ const title = document.getElementById('TitleHW2') as HTMLParagraphElement;
 const uploadDate = document.getElementById('Date') as HTMLParagraphElement;
 const alt = document.getElementById('Alt') as HTMLParagraphElement;
 
-function define(image : any ) : void {   
+function define(image : Comic ) : void {   
     img.setAttribute('src', image.img);
     img.setAttribute('alt', image.alt);
     title.textContent = image.title;
-    const date : string  = new Date(image.year, image.month, image.day).toLocaleDateString();
+    const date : string  = new Date(Number(image.year), Number(image.month), Number(image.day)).toLocaleDateString();
     uploadDate.textContent = "Date: " + date;
     alt.textContent = "Alt: " + image.alt;
 }
